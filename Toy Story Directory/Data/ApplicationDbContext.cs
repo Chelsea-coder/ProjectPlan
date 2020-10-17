37f6adcee839663bdac29e4772b6c2f63b1a878e
Chelsea <chelse_aa@live.ca>
2020-10-17 16:23:06 -0400

    Add project files.

---
+++ Toy Story Directory/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Toy_Story_Directory.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
